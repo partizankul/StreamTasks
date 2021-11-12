47976c63a58dc0f8a193628bb8491f2a46a75733
Alex Kuleshov <partizankul@gmail.com>
2021-11-12 10:16:37 +0300

    fix

--- src/main/java/TaskStream/SortByLetter.java
+++ src/main/java/TaskStream/SortByLetter.java
@@ -11,19 +11,22 @@ public class SortByLetter {
         List<String> country = listCountry.stream()
                 .filter(n -> n.equals(n.toLowerCase()))
                 .collect(Collectors.toList());
-        // country.forEach(System.out::println);
+        System.out.println("Страны написаные с маленькой буквы");
+         country.forEach(System.out::println);
+        System.out.println();
         List<String> COUNTRY = listCountry.stream()
                 .filter(n -> n.equals(n.toUpperCase()))
                 .collect(Collectors.toList());
-        // COUNTRY.forEach(System.out::println);
-        List<String> Country = listCountry.stream().
-                filter(n -> (String.valueOf(n.charAt(0))).equals(n.toUpperCase()))
+        System.out.println("Страны написаные большими буквами");
+         COUNTRY.forEach(System.out::println);
+        System.out.println();
+        List<String> Country = listCountry.stream()
+                .filter(n -> (String.valueOf(n.charAt(0))).equals(String.valueOf(n.charAt(0)).toUpperCase()))
+                .filter(n -> (String.valueOf(n.charAt(1))).equals(String.valueOf(n.charAt(1)).toLowerCase()))
                 .collect(Collectors.toList());
-
-        System.out.println(Country.isEmpty());
+        System.out.println("Страны написаныйе с большой буквы");
         Country.forEach(System.out::println);
-        //  String name = "NAme";
-        //   System.out.println(String.valueOf(name.charAt(0)));
+
 
     }
 }
